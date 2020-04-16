d81594c3968f1b589f4ab30c57648b104f107503
Immueggpain <Immueggpain@users.noreply.github.com>
2020-04-16 19:39:38 +0800

    fix empty bag slot

--- eat_bag.lua
+++ eat_bag.lua
@@ -263,10 +263,12 @@ local function sortBagsEasy(bank)
 	for i = #containers, 2, -1 do
 		local container = containers[i]
 		local bagItemID = GetInventoryItemID("player", ContainerIDToInventoryID(container))
-		local bagName = GetItemInfo(bagItemID)
-		local bagType = GetItemFamily(bagItemID)
-		--print(string.format("%s %d", bagName, bagType))
-		if bagType~=0 then table.remove(containers, i) end
+		if bagItemID ~= nil then
+			local bagName = GetItemInfo(bagItemID)
+			local bagType = GetItemFamily(bagItemID)
+			--print(string.format("%s %d", bagName, bagType))
+			if bagType~=0 then table.remove(containers, i) end
+		end
 	end
 	
 	--get all items into a dict
